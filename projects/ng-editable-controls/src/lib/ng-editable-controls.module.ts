3b0758b994daa41fc260742de6165abab9bbb4a2
prashantpimpale93 <prashantpimpale93@gmail.com>
2019-07-12 14:57:41 +0530

    Code intended

--- projects/ng-editable-controls/src/lib/ng-editable-controls.module.ts
+++ projects/ng-editable-controls/src/lib/ng-editable-controls.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { NgEditableControlsComponent } from './ng-editable-controls.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import {NgEditableControlsModule } from '../../projects/ng-editable-controls/src/lib/ng-editable-controls.module'
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgEditableControlsModule } from '../../projects/ng-editable-controls/src/lib/ng-editable-controls.module'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
